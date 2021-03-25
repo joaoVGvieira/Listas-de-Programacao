48cdc60404fe2ccc77d334b073319e0588d5e794
João Vitor Gonçalves Vieira <80349668+joaoVGvieira@users.noreply.github.com>
2021-03-25 12:25:33 -0300

    02.c
    Faça um algoritmo que preencha um vetor de 10 elementos inteiros,
    verifique a existência de elementos iguais a 5 e mostre as posições em
    que aparecem.

--- Lista-03-Vetores/02.c
+++ Lista-03-Vetores/02.c
@@ -1,3 +1,14 @@
-Faça um algoritmo que preencha um vetor de 10 elementos inteiros,
-verifique a existência de elementos iguais a 5 e mostre as posições em
-que aparecem.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int A[10];
+   int i;
+   for(i=0;i<10;i++){
+       scanf("%d",&A[i]);
+       if (A[i]==5){
+           printf("%d",i);
+       }
+   }
+}
